5f5140038372c2efc5c65558617025a3244b2dc9
Facu Sanchez <facusanchz96@gmail.com>
2024-03-01 19:39:24 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+backgroundImage:{
+"menuopen": "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRymaqkaL2zGrzlQaT0Bvymlz2KTarKW7yOYILwJ2GwMq4dZjmOaPeeywCWF2YfZlaSEXA&usqp=CAU')",
+"menuclose":"url('https://static.vecteezy.com/system/resources/previews/012/744/230/non_2x/close-button-icon-free-vector.jpg')"
+
+}
+
+    },
+  },
+  plugins: [],
+}
+
